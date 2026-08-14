3842aa45fcb8d64bd7ac03271c3c4c275243336b
mxrtnn <martinjara580@gmail.com>
2026-08-14 16:51:44 -0400

    Conexión TiDB Cloud

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+// Crear la conexión a MySQL
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  }
+});
